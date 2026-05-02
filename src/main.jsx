84f78e97b3d4648876f99ff16f38d0c7b8502091
aakashakash933-hue <aakashakash933@gmail.com>
2026-05-02 16:49:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import RedHotStore from './RedHotStore'
-import RedHotAdmin from './RedHotAdmin'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<RedHotStore />} />
-      <Route path="/admin" element={<RedHotAdmin />} />
-    </Routes>
-  </BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
